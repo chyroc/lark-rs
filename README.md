2a3123e585c84b3d7fb380e26882e30de471c2fe
chyroc <chyroc@qq.com>
2021-11-22 23:09:11 +0800

    dev

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # lark-rs
 
 Feishu/Lark Open API Rust SDK, Support ALL Open API and Event Callback.
+
+https://crates.io/crates/lark-rs
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+// use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct IPData {
+    origin: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp: IPData = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
