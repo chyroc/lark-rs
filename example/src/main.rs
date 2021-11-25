ea3ed15a8eef5fec82e0f78435be84d96973c630
chyroc <chyroc@qq.com>
2021-11-25 23:16:13 +0800

    fix

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,7 +1,20 @@
+use std::io::Error;
+
 use lark::Lark;
 
-
 fn main() {
-    Lark::new();
+    let ins = Lark::new(String::from(""), String::from(""));
+
+    let res = ins.get_token().unwrap_err();
+    println!("err: {}", res.message);
+    // match res {
+    //     Result(res) => {
+    //         println!("{:?}", res);
+    //     }
+    //     Err(e) => {
+    //         println!("{}", e.message);
+    //     }
+    // }
+    // block_on(ins.get_token());
     println!("Hello, world!");
 }
--- lark/src/lib.rs
+++ lark/src/lib.rs
@@ -1,11 +1,54 @@
-pub struct Lark {
+use std::collections::HashMap;
 
+use serde::{Deserialize, Serialize};
+
+use lark_error::Error;
+
+pub mod lark_error;
+
+// use lark_error::Error;
+
+pub struct Lark {
+    app_id: String,
+    app_secret: String,
 }
 
-impl Lark {
- pub fn new() -> Lark {
-        Lark {
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GetTokenRespData {}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GetTokenResp {
+    pub code: i32,
+    pub data: GetTokenRespData,
+    pub msg: String,
+}
+
+
+impl Lark {
+    pub fn new(app_id: String, app_secret: String) -> Lark {
+        Lark {
+            app_id,
+            app_secret,
+        }
+    }
+
+    pub fn get_token(&self) -> Result<GetTokenRespData, Error> {
+        println!("start get token");
+        let uri = String::from("https://open.feishu.cn/open-apis/auth/v3/tenant_access_token");
+        let mut map = HashMap::new();
+        map.insert("app_id", &self.app_id);
+        map.insert("app_secret", &self.app_secret);
+        let client = reqwest::blocking::Client::new();
+        let res = client.post(&uri).json(&map).send().unwrap();
+        let res2: GetTokenResp = res.json().unwrap();
+        if res2.code == 0 {
+            Ok(res2.data)
+        } else {
+            Err(Error {
+                code: res2.code,
+                message: res2.msg,
+            })
         }
     }
 }
